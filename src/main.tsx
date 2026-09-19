0d8c853fee3df9c0941fd1ebd88d0fe13046f69e
Javi3r66 <info@redvioletaciberprevencion.es>
2026-09-19 15:21:43 +0200

    Refactor router import and initialization logic

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
-import router from './router'
+import * as RouterModule from './router'
+
+// Detecta automáticamente si la exportación es default o nombrada (createRouter, getRouter, etc.)
+const routerInstance = 
+  RouterModule.default || 
+  RouterModule.router || 
+  (typeof RouterModule.createRouter === 'function' ? RouterModule.createRouter() : null) ||
+  (typeof RouterModule.getRouter === 'function' ? RouterModule.getRouter() : null)
 
 const rootElement = document.getElementById('root')!
-if (!rootElement.innerHTML) {
+if (!rootElement.innerHTML && routerInstance) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <React.StrictMode>
-      <RouterProvider router={router} />
+      <RouterProvider router={routerInstance} />
     </React.StrictMode>
   )
 }
