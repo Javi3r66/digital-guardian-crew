b528e8b54f8373e4ca1a51dee7705318d75e00f5
Javi3r66 <info@redvioletaciberprevencion.es>
2026-09-19 04:48:28 +0200

    Update stylesheet import and add meta tags

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,10 +1,12 @@
 import { createRootRoute, HeadContent, Scripts, Outlet } from '@tanstack/react-router'
-import appCss from '../index.css?url'
+import '../index.css'
 
 export const Route = createRootRoute({
   head: () => ({
-    links: [
-      { rel: 'stylesheet', href: appCss },
+    meta: [
+      { charSet: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { title: 'Red Violeta Ciberprevención' },
     ],
   }),
   component: RootComponent,
