3a511ceaf44f602f3d26715313e9ff7476162a60
Javi3r66 <info@redvioletaciberprevencion.es>
2026-09-19 10:38:25 +0200

    Add Footer component to root route

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,6 @@
 import { createRootRoute, HeadContent, Scripts, Outlet } from '@tanstack/react-router'
 import appCss from '../styles.css?url'
+import { Footer } from '../components/Footer'
 
 export const Route = createRootRoute({
   head: () => ({
@@ -23,6 +24,7 @@ function RootComponent() {
       </head>
       <body>
         <Outlet />
+        <Footer />
         <Scripts />
       </body>
     </html>
