bf4247e4018a82217416945cda86f3f71e78e3d8
Javi3r66 <info@redvioletaciberprevencion.es>
2026-09-21 19:33:15 +0200

    Refactor RootComponent to use Link and inline styles

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,20 +1,17 @@
-import '../styles.css'
-import { createRootRoute, Outlet } from '@tanstack/react-router'
-import { SiteHeader } from '../components/SiteHeader'
-import { SiteFooter } from '../components/SiteFooter'
+import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 
 export const Route = createRootRoute({
-  component: RootComponent,
-})
-
-function RootComponent() {
-  return (
-    <div className="min-h-screen flex flex-col">
-      <SiteHeader />
-      <main className="flex-1">
+  component: () => (
+    <div style={{ padding: '20px', fontFamily: 'system-ui, sans-serif' }}>
+      <header style={{ marginBottom: '20px' }}>
+        <Link to="/" style={{ fontWeight: 'bold', color: '#2563eb' }}>
+          Inicio
+        </Link>
+      </header>
+      <hr style={{ borderColor: '#e5e7eb' }} />
+      <main style={{ marginTop: '20px' }}>
         <Outlet />
       </main>
-      <SiteFooter />
     </div>
-  )
-}
+  ),
+})
